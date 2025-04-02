ca8b17acc535c48fe338dc9b73c1330ce9781f2f
andrewbui04 <thaianhbk1@gmail.com>
2025-04-02 09:45:09 +0700

    test:Test pipeline-v3

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
             // If both tables exist
             if(mysqli_num_rows($checkTable1) > 0 && mysqli_num_rows($checkTable2) > 0) {
-                echo "<p>dmmVuntra</p>";
+                echo "<p>dmmVuntra as Huy VU</p>";
             } else {
                 // If not, create the tables and populate them
                 $query = 
